e9b982b05f32b1913714d49bf1a3e408c8eaaf9a
vili22 <aethiopicus@hotmail.com>
2017-04-27 20:06:47 +0300

    basic functionality for 3d drawing initialized

--- src/map/glwidget.cpp
+++ src/map/glwidget.cpp
@@ -1,53 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-// Include GLEW
 #include <GL/glew.h>
 #include "glwidget.h"
 #include <QMouseEvent>
@@ -61,34 +11,14 @@
 using namespace glm;
 
 GLWidget::GLWidget(QWidget *parent)
-    : QOpenGLWidget(parent)
-{
+    : QOpenGLWidget(parent), horizontalAngle(3.14f), verticalAngle(0.0f),
+	  mouseSpeed(0.005f), cameraPosition(5,2,10), prevPoint(-1, -1) {
 
+	setMouseTracking(true);
 }
 
-GLWidget::~GLWidget()
-{
+GLWidget::~GLWidget() {
     cleanup();
-}
-
-QSize GLWidget::minimumSizeHint() const
-{
-    return QSize(50, 50);
-}
-
-QSize GLWidget::sizeHint() const
-{
-    return QSize(400, 400);
-}
-
-void GLWidget::cleanup()
-{
-    makeCurrent();
-    // Cleanup VBO
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
-	glDeleteProgram(programID);
-    doneCurrent();
 }
 
 static void createInitialData(GLfloat *data) {
@@ -134,37 +64,53 @@ static void createInitialData(GLfloat *data) {
     data[38] = -3.0f;
 }
 
-void GLWidget::initializeGL()
+static void printMatrix(glm::mat4 matrix) {
+
+    for(int k = 0; k < 4; k++) {
+        for(int l = 0; l < 4; l++) {
+            std::cout << matrix[k][l] << " ";
+        }
+        std::cout << "\n\n";
+    }
+}
+
+QSize GLWidget::minimumSizeHint() const {
+
+	return QSize(50, 50);
+}
+
+QSize GLWidget::sizeHint() const {
+
+	return QSize(600, 600);
+}
+
+void GLWidget::cleanup()
 {
-    glewExperimental = GL_TRUE;
+    makeCurrent();
+    // Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
+    doneCurrent();
+}
+
+void GLWidget::initializeGL() {
+
+	glewExperimental = GL_TRUE;
     glewInit();
 	glClearColor(0.0f, 0.0f, 0.1f, 0.0f);
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
     programID = LoadShaders( "/home/vvirkkal/Documents/development/rover-controller/src/map/SimpleVertexShader.vertexshader", "/home/vvirkkal/Documents/development/rover-controller/src/map/SimpleFragmentShader.fragmentshader" );
-
     matrixID = glGetUniformLocation(programID, "MVP");
 
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-    // Camera matrix
-    glm::mat4 View = glm::lookAt(glm::vec3(0,10,10), // Camera is at (4,3,3), in World Space
-                                 glm::vec3(0,0,0), // and looks at the origin
-                                 glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-                                 );
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 Model = glm::mat4(1.0f);
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
-
+    updateMatrices();
 
     createInitialData(g_vertex_buffer_data);
 
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 }
 
@@ -190,5 +136,91 @@ void GLWidget::paintGL() {
     // Draw the triangle !
     glDrawArrays(GL_LINE_STRIP, 0, len_buffer/3); // Starting from vertex 0; 3 vertices total -> 1 triangle
     glDisableVertexAttribArray(0);
+}
+
+void GLWidget::translate(float delta_x, float delta_y, float delta_z) {
+
+	cameraPosition += glm::vec3(delta_x, delta_y, delta_z);
+	updateMatrices();
+    update();
+}
+
+void GLWidget::mousePositionChanged(int x, int y) {
+
+	if(prevPoint.x == -1) {
+		prevPoint.x = x;
+		prevPoint.y = y;
+		return;
+	}
+
+	horizontalAngle += mouseSpeed * float(prevPoint.x - x);
+	verticalAngle   += mouseSpeed * float(prevPoint.y - y );
+
+	prevPoint.x = x;
+	prevPoint.y = y;
+	updateMatrices();
+	update();
+}
+
+void GLWidget::mouseMoveEvent(QMouseEvent *e) {
+	mousePositionChanged(e->x(), e->y());
+}
+
+void GLWidget::updateDirectionVectors() {
+
+	calcFrontVector();
+	calcRightVector();
+	calcUpVector();
+}
+
+void GLWidget::calcFrontVector() {
+
+	frontVector = glm::vec3(
+			cos(verticalAngle) * sin(horizontalAngle),
+			sin(verticalAngle),
+			cos(verticalAngle) * cos(horizontalAngle)
+			);
+}
+
+void GLWidget::calcRightVector() {
+
+	 rightVector = glm::vec3(
+			 sin(horizontalAngle - 3.14f/2.0f),
+			 0,
+			 cos(horizontalAngle - 3.14f/2.0f)
+			 );
+}
+
+void GLWidget::calcUpVector() {
+
+	upVector = glm::cross( rightVector, frontVector);
+}
+
+void GLWidget::updateMatrices() {
+
+	updateDirectionVectors();
+	updateProjectionMatrix();
+	updateViewMatrix();
+	updateModelMatrix();
+
+	mvp = projectionMatrix * viewMatrix * modelMatrix;
+}
+
+void GLWidget::updateProjectionMatrix() {
+
+	 projectionMatrix = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+}
+
+void GLWidget::updateViewMatrix() {
+
+	 viewMatrix = glm::lookAt(cameraPosition,
+			 	 	 	 	 cameraPosition + frontVector,
+							 upVector
+							 );
+}
+
+void GLWidget::updateModelMatrix() {
+
+	 modelMatrix = glm::mat4(1.0f);
 }
 
